51b0652eb448f945adb0d9c4677e34ec26b46c82
TomasFranchino <108561595+TomasFranchino@users.noreply.github.com>
2024-10-02 22:35:00 -0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppVeterinaria.Models;
+
+namespace WebAppVeterinaria.Data
+
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }  
+        public DbSet<Animal> Animal { get; set; }
+        public DbSet<Dueño> Dueño { get; set; }
+        public DbSet<Atencion> Atenciones {  get; set; }  
+    }
+}
